16c4977eaf3c1672878b172bc48084417b94b75c
ne0liberal <104116505+ne0liberal@users.noreply.github.com>
2022-04-23 17:15:34 -0500

    Update importer_manual.py

--- importer_manual.py
+++ importer_manual.py
@@ -1,7 +1,8 @@
 import os, shutil
 
 """this is my alt for when i cant automatically use fuzzy matching to find the correct folder to import to
-it just iterates thru every folder you have downloaded and then asks for the full path to move said folders contents to"""
+it just iterates thru every folder you have downloaded and then asks for the full path to move said folders contents to.
+also this is deprecated, and i wont be cleaning it up"""
 
 def move_files(input_dir, output_dir):
     for root, dirs, files in os.walk(input_dir):
